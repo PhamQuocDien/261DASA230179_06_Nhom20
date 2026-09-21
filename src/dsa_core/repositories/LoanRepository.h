e75bc9eb75a0acb69a06038b793ae753b32db0c2
letrongdoan <168190924+letrongdoan@users.noreply.github.com>
2026-09-21 18:46:13 +0700

    Update LoanRepository.h

--- src/dsa_core/repositories/LoanRepository.h
+++ src/dsa_core/repositories/LoanRepository.h
@@ -12,6 +12,8 @@ public:
     const std::vector<Loan>& getAll() const;
     Loan* findById(const std::string& loanId);
     const Loan* findById(const std::string& loanId) const;
+    Loan* findByMemberAndBook(const std::string& memberId, const std::string& bookId);
+    const Loan* findByMemberAndBook(const std::string& memberId, const std::string& bookId) const;
     bool add(const Loan& loan);
     bool update(const Loan& loan);
     bool removeById(const std::string& loanId);
