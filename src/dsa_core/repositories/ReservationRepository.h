a40cacfbc2161dc9d222edbccf6f145b3d651ecb
PhamQuocDien <dienp3030@gmail.com>
2026-09-22 09:19:36 +0700

    Format ReservationRepository.h for consistency

--- src/dsa_core/repositories/ReservationRepository.h
+++ src/dsa_core/repositories/ReservationRepository.h
@@ -1,18 +1,20 @@
 #pragma once
+
 #include "Reservation.h"
 #include <vector>
 using namespace std;
 
 class ReservationRepository {
-	private:
-		vector<Reservation> reservations;
-	public:
-		void add(const Reservation& reservation);
-		Reservation* findById(const string& reservationId);
-		bool update(const Reservation& reservation);
-		bool remove(const string& reservationId);
-		vector<Reservation>& getAll();
+private:
+    vector<Reservation> reservations;
 
-		Reservation* findByMemberAndBook(const string& memberId, const string& bookId);
-		
+public:
+    void add(const Reservation& reservation);
+    Reservation* findById(const string& reservationId);
+    bool update(const Reservation& reservation);
+    bool remove(const string& reservationId);
+    vector<Reservation>& getAll();
+
+    Reservation* findByMemberAndBook( const string& memberId,const string& bookCode
+    );
 };
