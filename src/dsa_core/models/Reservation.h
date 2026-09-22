c910b5ceb227bd8003650e8ade03b887ed545ec7
PhamQuocDien <dienp3030@gmail.com>
2026-09-22 10:11:46 +0700

    Refactor Reservation.h with include guards and bookCode
    Replaced pragma once with include guards and added bookCode field.

--- src/dsa_core/models/Reservation.h
+++ src/dsa_core/models/Reservation.h
@@ -1,12 +1,22 @@
-#pragma once
-#include <string>
-using namespace std;
-struct Reservation {
-	string reservationId;
-	string memberId;
-	string bookId;
-	string reservationDate;
-	string status;
+#ifndef RESERVATION_H
+#define RESERVATION_H
 
+#include <string>
+
+using namespace std;
+
+struct Reservation
+{
+    string reservationId;
+
+    string memberId;
+
+    // Reservation theo mã đầu sách
+    string bookCode;
+
+    string reservationDate;
+
+    string status;
 };
-string getCurrentTime();
+
+#endif
