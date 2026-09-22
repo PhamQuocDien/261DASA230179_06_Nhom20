8fff8bd17b62ff710a7f2cd538285c13bd619850
PhamQuocDien <dienp3030@gmail.com>
2026-09-22 12:35:21 +0700

    up

--- src/dsa_core/repositories/LoanRepository.h
+++ src/dsa_core/repositories/LoanRepository.h
@@ -1,20 +1,32 @@
 #ifndef LOANREPOSITORY_H
 #define LOANREPOSITORY_H
+
 #include <string>
 #include <vector>
+
 #include "../models/Loan.h"
+
 class LoanRepository {
 private:
     std::vector<Loan> loans;
+
 public:
     LoanRepository();
+
     std::vector<Loan>& getAll();
     const std::vector<Loan>& getAll() const;
+
     Loan* findById(const std::string& loanId);
     const Loan* findById(const std::string& loanId) const;
+
+    const Loan* findByMemberAndBook(
+        const std::string& memberId,
+        const std::string& bookId
+    ) const;
 
     bool add(const Loan& loan);
     bool update(const Loan& loan);
     bool removeById(const std::string& loanId);
 };
+
 #endif
