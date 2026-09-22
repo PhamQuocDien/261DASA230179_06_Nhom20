7acce02f13f9b4d3e98576a7ca4170cb318e4711
PhamQuocDien <dienp3030@gmail.com>
2026-09-22 10:21:57 +0700

    Refactor ReservationRepository header file

--- src/dsa_core/repositories/ReservationRepository.h
+++ src/dsa_core/repositories/ReservationRepository.h
@@ -1,18 +1,41 @@
-#pragma once
-#include "Reservation.h"
+#ifndef RESERVATIONREPOSITORY_H
+#define RESERVATIONREPOSITORY_H
+
+#include "../models/Reservation.h"
+
+#include <string>
 #include <vector>
+
 using namespace std;
 
-class ReservationRepository {
-	private:
-		vector<Reservation> reservations;
-	public:
-		void add(const Reservation& reservation);
-		Reservation* findById(const string& reservationId);
-		bool update(const Reservation& reservation);
-		bool remove(const string& reservationId);
-		vector<Reservation> getAll();
+class ReservationRepository
+{
+private:
+    vector<Reservation> reservations;
 
-		Reservation* findByMemberAndBook(const string& memberId, const string& bookId);
-		
+public:
+    void add(
+        const Reservation& reservation
+    );
+
+    Reservation* findById(
+        const string& reservationId
+    );
+
+    bool update(
+        const Reservation& reservation
+    );
+
+    bool remove(
+        const string& reservationId
+    );
+
+    vector<Reservation>& getAll();
+
+    Reservation* findByMemberAndBook(
+        const string& memberId,
+        const string& bookCode
+    );
 };
+
+#endif
