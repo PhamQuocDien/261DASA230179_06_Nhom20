d188c6da9c08e471b91a3e16745bbbfb229502f6
pngbao2007-hue <pngbao2007@gmail.com>
2026-09-22 08:39:56 +0700

    Add files via upload

--- src/dsa_core/models/Book.h
+++ src/dsa_core/models/Book.h
@@ -1,19 +1,74 @@
 #ifndef BOOK_H
 #define BOOK_H
+
+#include <iostream>
 #include <string>
-#include <vector>
+
 using namespace std;
-struct BookCopy {
-    string bookId;
-    string status;
-};
-struct Book {
-    string bookCode;
-    string title;
-    string author;
-    string category;
-    int year;
-    vector<BookCopy> copies;
+
+class Book {
+public:
+
+    // Mã đầu sách
+    string BookCode;
+
+    // ID duy nhất của bản sách
+    string Book_ID;
+
+    string Title;
+    string Author;
+    string Category;
+
+    int Year;
+    int Quantity;
+
+    // Số lượt được quan tâm
+    int interestCount;
+
+
+    Book() {
+        Year = 0;
+        Quantity = 0;
+        interestCount = 0;
+    }
+
+
+    Book(
+        string code,
+        string id,
+        string title,
+        string author,
+        string category,
+        int year,
+        int quantity
+    ) {
+
+        BookCode = code;
+        Book_ID = id;
+
+        Title = title;
+        Author = author;
+        Category = category;
+
+        Year = year;
+        Quantity = quantity;
+
+        interestCount = 0;
+    }
+
+
+    void display() const {
+
+        cout << "Ma sach       : " << BookCode << endl;
+        cout << "Book_ID       : " << Book_ID << endl;
+        cout << "Ten sach      : " << Title << endl;
+        cout << "Tac gia       : " << Author << endl;
+        cout << "The loai      : " << Category << endl;
+        cout << "Nam xuat ban  : " << Year << endl;
+        cout << "So luong      : " << Quantity << endl;
+        cout << "Luot quan tam : " << interestCount << endl;
+    }
 };
 
 #endif
+
